ebb7d3bcd5ec23a79f4cba792417556030ecf82a
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2021-11-29 14:06:04 +0700

    remove codepen profile link

--- src/config.js
+++ src/config.js
@@ -18,10 +18,6 @@ module.exports = {
       name: 'Linkedin',
       url: 'https://www.linkedin.com/in/m-algah-fattah-illahi/',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
   ],
 
   navLinks: [
