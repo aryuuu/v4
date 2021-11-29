62ebf3cebeea621eea65aad5df8be26931931d41
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2021-11-29 13:26:24 +0700

    update gatsby-config to match personal profile

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,11 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'M Algah Fattah Illahi',
+    description: 'M Algah Fattah Illahi is a software engineer',
+    siteUrl: 'https://aryuuu.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@fattfatt10',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
